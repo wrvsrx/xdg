0b81f04b18c6a021679a239602f66fe25604608d
wrvsrx <wrvsrx@outlook.com>
2026-04-10 02:53:51 +0800

    chore(env): update build logic

--- default.nix
+++ default.nix
@@ -1,16 +1,8 @@
 {
-  stdenv,
-  lean4,
-  lakeSetupHook,
-  writeText,
+  leanPackages,
 }:
-stdenv.mkDerivation {
+leanPackages.buildLakePackage {
   pname = "xdg";
-  version = "0.2.0";
+  version = "0.3.0";
   src = builtins.path { path = ./.; };
-  env.NIX_LAKE_MANIFEST_OVERRIDE = writeText "lake-manifest-override.json" (builtins.toJSON [ ]);
-  nativeBuildInputs = [
-    lean4
-    lakeSetupHook
-  ];
 }
