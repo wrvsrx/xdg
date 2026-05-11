0a365bc58d545cbb76811a1957095d8d76b5a906
wrvsrx <wrvsrx@outlook.com>
2026-05-11 22:15:23 +0800

    chore: bump version to 0.9.0-dev

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 leanPackages.buildLakePackage {
   pname = "xdg";
-  version = "0.8.0";
+  version = "0.9.0-dev";
   src = builtins.path { path = ./.; };
   lakeHash = "sha256-cyZz+1PsuCaI2Pr5jgsv4Dv003oNXgy/DUa0lnUkBGY=";
 
